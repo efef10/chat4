d130e4ea7f165fb8963101b51f6ce0f19751e6aa
efef10 <co.efrat10@gmail.com>
2018-06-04 10:53:29 +0300

    username

--- src/models/Chat.ts
+++ src/models/Chat.ts
@@ -15,28 +15,29 @@ export class Chat{
 
         // example initials
         this.users.addUser("efrat",25,"efrat");
-        this.users.addUser("gal",25,"gal");
+        this.users.addUser("eli",1,"eli");
         this.users.addUser("bambi",3,"bambi","bambi.jpg");
         this.users.addUser("simba",3,"simba","simba.jpg");
         this.groups.addInitialGroup("jsbootcamp3");
         this.addUserToGroup("simba","jsbootcamp3");
         this.addUserToGroup("efrat","jsbootcamp3");
-        this.addUserToGroup("gal","jsbootcamp3");
+        this.addUserToGroup("eli","jsbootcamp3");
         this.addUserToGroup("bambi","jsbootcamp3");
         this.addGroup("react","jsbootcamp3");
         this.addUserToGroup("efrat","jsbootcamp3>react");
-        this.addUserToGroup("gal","jsbootcamp3>react");
+        this.addUserToGroup("eli","jsbootcamp3>react");
         this.addUserToGroup("bambi","jsbootcamp3>react");
         this.addMessageToGroup("jsbootcamp3>others","bambi","welcome to others group");
         this.addMessageToGroup("jsbootcamp3>others","efrat","what a great group!");
         this.addMessageToGroup("jsbootcamp3>others","efrat","with such a meaningfull name.... who had the idea to call a group 'others'???");
-        this.addMessageToGroup("jsbootcamp3>others","gal","it was mine! don't you like it? you are more than welcome to think about a new cool name for us...");
+        this.addMessageToGroup("jsbootcamp3>others","eli","it was mine! don't you like it? you are more than welcome to think about a new cool name for us...");
         this.addMessageToGroup("jsbootcamp3>others","efrat",
             "VeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTestVeryLongMessageTest");
         this.addMessageToGroup("jsbootcamp3>react","efrat","welcome to our group");
         let user = this.returnUserByName("efrat");
         if(!!user) {
-            user.addMessage({content:"hi gal, it's efrat",date:new Date(),userName:"efrat",chattingWithUser:"gal"})
+            user.addMessage({content:"hi eli, it's efrat",date:new Date(),userName:"efrat",chattingWithUser:"eli"})
+            user.addMessage({content:"hi bambi, it's efrat",date:new Date(),userName:"efrat",chattingWithUser:"bambi"})
         }
         debugger
         this.addGroup("group3","jsbootcamp3>react");
